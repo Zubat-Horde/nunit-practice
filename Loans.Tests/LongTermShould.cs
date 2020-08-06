5443e583a42e3b898e76a44ed343fd77ea2d8953
Christian Jensen <38145685+Zubat-Horde@users.noreply.github.com>
2020-08-05 21:40:54 -0500

    added test library and two test methods

---
+++ Loans.Tests/LongTermShould.cs
@@ -0,0 +1,24 @@
+﻿using Loans.Domain.Applications;
+using NUnit.Framework;
+namespace Loans.Tests
+{
+    [TestFixture]
+    public class LongTermShould
+    {
+        [Test]
+        public void ReturnTermInMonths()
+        {
+            var sut = new LoanTerm(1);
+
+            Assert.That(sut.ToMonths(), Is.EqualTo(12));
+        }
+
+        [Test]
+        public void StoreYears()
+        {
+            var sut = new LoanTerm(1);
+
+            Assert.That(sut.Years, Is.EqualTo(1));
+        }
+    }
+}
