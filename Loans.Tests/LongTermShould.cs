0bc7cb83290e79a946cbdec1a7c6c6c5f56f53d8
Christian Jensen <38145685+Zubat-Horde@users.noreply.github.com>
2020-08-06 11:50:10 -0500

    asserting on equality

--- Loans.Tests/LongTermShould.cs
+++ Loans.Tests/LongTermShould.cs
@@ -20,5 +20,23 @@ namespace Loans.Tests
 
             Assert.That(sut.Years, Is.EqualTo(1));
         }
+
+        [Test]
+        public void RespectValueEquality()
+        {
+            var a = new LoanTerm(1);
+            var b = new LoanTerm(1);
+
+            Assert.That(a, Is.EqualTo(b));
+        }
+
+        [Test]
+        public void RespectValueInequality()
+        {
+            var a = new LoanTerm(1);
+            var b = new LoanTerm(2);
+
+            Assert.That(a, Is.Not.EqualTo(b));
+        }
     }
 }
