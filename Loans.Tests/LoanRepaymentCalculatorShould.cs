0abeff22df9a1c00fc402bb207c5f3fb49ff5cd5
Christian Jensen <38145685+Zubat-Horde@users.noreply.github.com>
2020-08-07 15:12:36 -0500

    learned how to generate test data

--- Loans.Tests/LoanRepaymentCalculatorShould.cs
+++ Loans.Tests/LoanRepaymentCalculatorShould.cs
@@ -62,5 +62,45 @@ namespace Loans.Tests
 
             Assert.That(monthlyPayment, Is.EqualTo(expectedMonthlyPayment));
         }
+
+        [Test]
+        public void CalculateCorrectMonthlyRepayment_Combinatorial(
+            [Values(100_000, 200_000, 500_000)] decimal principal,
+            [Values(6.5, 10, 20)] decimal interestRate,
+            [Values(10, 20, 30)] int termInYears)
+        {
+            var sut = new LoanRepaymentCalculator();
+
+            var monthlyPayment = sut.CalculateMonthlyRepayment(
+                new LoanAmount("USD", principal), interestRate, new LoanTerm(termInYears));
+        }
+
+        [Test]
+        [Sequential]
+        public void CalculateCorrectMonthlyRepayment_Sequential(
+                    [Values(200_000, 200_000, 500_000)] decimal principal,
+                    [Values(6.5, 10, 10)] decimal interestRate,
+                    [Values(30, 30, 30)] int termInYears,
+                    [Values(1264.14, 1755.14, 4387.86)] decimal expectedMonthlyPayment)
+        {
+            var sut = new LoanRepaymentCalculator();
+
+            var monthlyPayment = sut.CalculateMonthlyRepayment(
+                new LoanAmount("USD", principal), interestRate, new LoanTerm(termInYears));
+
+            Assert.That(monthlyPayment, Is.EqualTo(expectedMonthlyPayment));
+        }
+
+        [Test]
+        public void CalculateCorrectMonthlyRepayment_Range(
+           [Range(50_000, 1_000_000, 50_000)] decimal principal,
+           [Range(0.5, 20.00, 0.5)] decimal interestRate,
+           [Values(10, 20, 30)] int termInYears)
+        {
+            var sut = new LoanRepaymentCalculator();
+
+            sut.CalculateMonthlyRepayment(
+                new LoanAmount("USD", principal), interestRate, new LoanTerm(termInYears));
+        }
     }
 }
